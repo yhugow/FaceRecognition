9c784ef65201e4157eee6487fae1922c8015c55c
ITSupport <jhalicante-ctl@abs-cbn.com>
2019-02-15 18:07:34 +0800

    Create API

--- faceRecognition.py
+++ faceRecognition.py
@@ -9,9 +9,9 @@ import numpy as np
 def faceDetection(test_img):
     gray_img=cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)#convert color image to grayscale
     face_haar_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')#Load haar classifier
-    faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.32,minNeighbors=5)#detectMultiScale returns rectangles
+    faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.3,minNeighbors=3)#detectMultiScale returns rectangles
 
-    return faces,gray_img
+    return faces,gray_img 
 
 #Given a directory below function returns part of gray_img which is face alongwith its label/ID
 def labels_for_training_data(directory):
@@ -21,16 +21,16 @@ def labels_for_training_data(directory):
     for path,subdirnames,filenames in os.walk(directory):
         for filename in filenames:
             if filename.startswith("."):
-                print("Skipping system file")#Skipping files that startwith .
+                #print("Skipping system file")#Skipping files that startwith .
                 continue
 
             id=os.path.basename(path)#fetching subdirectory names
             img_path=os.path.join(path,filename)#fetching image path
-            print("img_path:",img_path)
-            print("id:",id)
+            #print("img_path:",img_path)
+            #print("id:",id)
             test_img=cv2.imread(img_path)#loading each image one by one
             if test_img is None:
-                print("Image not loaded properly")
+                #print("Image not loaded properly")
                 continue
             faces_rect,gray_img=faceDetection(test_img)#Calling faceDetection function to return faces detected in particular image
             if len(faces_rect)!=1:
@@ -40,7 +40,6 @@ def labels_for_training_data(directory):
             faces.append(roi_gray)
             faceID.append(int(id))
     return faces,faceID
-
 
 #Below function trains haar classifier and takes faces,faceID returned by previous function as its arguments
 def train_classifier(faces,faceID):
--- tester.py
+++ tester.py
@@ -2,40 +2,83 @@ import cv2
 import os
 import numpy as np
 import faceRecognition as fr
+import urllib
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+from flask_cors import CORS
+
+app = Flask(__name__)   
+api = Api(app)
+CORS(app)
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
+
+class User(Resource):
+    def get(self):
+
+        #This module takes images  stored in diskand performs face recognition
+        test_img=cv2.imread('/Users/itsupport/Documents/IT/Herbal App/app/fr/FaceRecognition/TestImages/img2.jpg')#test_img path
+        faces_detected,gray_img=fr.faceDetection(test_img)
+        print("faces_detected:",faces_detected) 
 
 
-#This module takes images  stored in diskand performs face recognition
-test_img=cv2.imread('Tutorial_faceRecognition/TestImages/neha1.jpg')#test_img path
-faces_detected,gray_img=fr.faceDetection(test_img)
-print("faces_detected:",faces_detected)
+        #Uncomment belows lines when running this program first time.Since it svaes training.yml file in directory
+        faces,faceID=fr.labels_for_training_data('/Users/itsupport/Documents/IT/Herbal App/app/fr/FaceRecognition/trainingImages')
+        face_recognizer=fr.train_classifier(faces,faceID)
+        # face_recognizer.save('trainingData.yml')
+        # face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+        # face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
 
+        name={
+            0:"Old Man",
+            1:"Girl",
+            2:"Leonardo"
+        } #creating dictionary containing names for each label
 
-#Uncomment belows lines when running this program first time.Since it svaes training.yml file in directory
-# faces,faceID=fr.labels_for_training_data('Tutorial_faceRecognition/trainingImages')
-# face_recognizer=fr.train_classifier(faces,faceID)
-# face_recognizer.save('trainingData.yml')
-face_recognizer=cv2.face.LBPHFaceRecognizer_create()
-face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
+        resname =''
 
-name={0:"Priyanka",1:"Neha"}#creating dictionary containing names for each label
+        for face in faces_detected:
+            (x,y,w,h)=face
+            roi_gray=gray_img[y:y+h,x:x+h]
+            label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
+            print(label,confidence)
+            resname=label
+            fr.draw_rect(test_img,face)
+            predicted_name=name[label]
+            if(confidence>37):#If confidence less than 37 then don't print predicted face text on screen
+                continue
+            # fr.put_text(test_img,predicted_name,x,y)
 
-for face in faces_detected:
-    (x,y,w,h)=face
-    roi_gray=gray_img[y:y+h,x:x+h]
-    label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
-    print("confidence:",confidence)
-    print("label:",label)
-    fr.draw_rect(test_img,face)
-    predicted_name=name[label]
-    if(confidence>37):#If confidence less than 37 then don't print predicted face text on screen
-        continue
-    fr.put_text(test_img,predicted_name,x,y)
+        # resized_img=cv2.resize(test_img,(500,250))
+        # cv2.imshow("face dtecetion tutorial",resized_img)
+        # cv2.waitKey(0)#Waits indefinitely until a key is pressed
+        # cv2.destroyAllWindows
+        print(predicted_name)
+        print(label)
+        print(confidence)
+        return predicted_name, 200
+      
+api.add_resource(User, "/user")
 
-resized_img=cv2.resize(test_img,(1000,1000))
-cv2.imshow("face dtecetion tutorial",resized_img)
-cv2.waitKey(0)#Waits indefinitely until a key is pressed
-cv2.destroyAllWindows
-
+app.run(debug=True)
 
 
 
